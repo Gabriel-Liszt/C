6a47eb86d8222030c14931b8f2fb11919232d6a2
Gabriel Liszt Ferreira de Oliveira <141736171+Gabriel-Liszt@users.noreply.github.com>
2024-04-08 20:18:10 -0300

    revisao

--- ex_facul/prova2/vetores/ex013.c
+++ ex_facul/prova2/vetores/ex013.c
@@ -3,9 +3,9 @@
 void procurarelemento(int vetor[], int elemento, int T){
     for(int i = 0; i < T; i++){
         if(vetor[i] == elemento){
-            printf("Elemento encontrado no indice %d, com endereco de mem %d", i, (void *)&vetor[i]);
+            printf("Elemento encontrado no indice %d, com endereco de mem %d\n", i, (void *)&vetor[i]);
         }else{
-            printf("Elemento nao encontrado no indice %d.", i);
+            printf("Elemento nao encontrado no indice %d.\n", i);
         }
     }
 }
@@ -13,8 +13,7 @@ void procurarelemento(int vetor[], int elemento, int T){
 int main(){
     int vetor[t] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-    procurarelemento(vetor, 2, t);
-
+    procurarelemento(vetor, 20, t);
 
     return 0;
 }
