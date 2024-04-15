78fd7e3072bb47c8ed5889020957d5ae207a2c40
Gabriel Liszt Ferreira de Oliveira <141736171+Gabriel-Liszt@users.noreply.github.com>
2024-04-15 15:16:34 -0300

    casting

---
+++ C_pragramming/aula041/aula041.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    int x = 16;
+    int y = 3;
+    
+    //casting = (float), e pode ser qualquer tipode variavel.
+    float resultado = (float)x / y;
+
+    printf("%f\n", resultado);
+
+    return 0;
+}
